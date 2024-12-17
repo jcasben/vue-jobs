4f67dcdbac815dea4c0dd111e4ff12b08e3bf45e
jcasben <jcasben02@gmail.com>
2024-12-17 17:49:32 +0100

    [NEW]: initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+import Hero from "@/components/Hero.vue";
+</script>
+
+<template>
+  <Hero/>
+  <HomeCards/>
+  <JobListings :limit="3" :show-button="true" />
+</template>
